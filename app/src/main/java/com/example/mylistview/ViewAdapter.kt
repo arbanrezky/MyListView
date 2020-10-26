270df2e057ce4873abd5873c909338359ca18f12
arbanrezky <arbanrezky123@gmail.com>
2020-10-26 20:00:28 +0700

    MyListview

---
+++ app/src/main/java/com/example/mylistview/ViewAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.mylistview
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+class ViewAdapter {
+
+    private inner class ViewHolder internal constructor(view: View) {
+        private val txtName: TextView = view.findViewById(R.id.txt_name)
+        private val txtDescription: TextView = view.findViewById(R.id.txt_description)
+        private val imgPhoto: ImageView = view.findViewById(R.id.img_photo)
+        internal fun bind(hero: Hero) {
+            txtName.text = hero.name
+            txtDescription.text = hero.description
+            imgPhoto.setImageResource(hero.photo)
+        }
+    }
+}
